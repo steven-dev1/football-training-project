aa47f7e8535d98a2e09312c3d7dff57f8b2e6cca
Surialpha <sgp_96@gmail.com>
2024-08-31 12:29:59 -0500

    Poniento archivos en su lugar

---
+++ infrastructure/utils/helpers.ts
@@ -0,0 +1,70 @@
+import { CountriesWithLeagues } from "@/types/GameData";
+
+export const formatDate = (dateString: string): string => {
+  const date = new Date(dateString);
+  const months = [
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+  ];
+
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${day} ${month} ${year}`;
+};
+
+export const convertTimeToLocal = (timeString: string, timeZone: string): string => {
+  if (!timeString || !timeZone) return '00:00'
+  // Crear una fecha ficticia añadiendo la hora
+  const [hours, minutes] = timeString.split(':').map(Number);
+
+  // Suponiendo que la hora es en UTC (o ajustable)
+  const utcDate = new Date(Date.UTC(2024, 7, 29, hours - 2, minutes)); // Añadir cualquier fecha válida
+
+  // Convertir a la hora local usando Intl.DateTimeFormat
+  return new Intl.DateTimeFormat('en-US', {
+    timeZone,
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(utcDate);
+};
+
+export const groupLeaguesByCountry = (items: any[]): CountriesWithLeagues[] => {
+  const grouped = items.reduce((acc: Record<number, CountriesWithLeagues>, item: any) => {
+    const countryId = item.country_id;
+
+    // Si el país ya existe en el acumulador, le agregamos la nueva liga
+    if (!acc[countryId]) {
+      acc[countryId] = {
+        country: {
+          id: item.country_id,
+          name: item.country_name,
+          logo: item.country_logo,
+        },
+        leagues: [],
+      };
+    }
+
+    // Agregar la liga correspondiente al país
+    acc[countryId].leagues.push({
+      id: item.league_id,
+      name: item.league_name,
+      logo: item.league_logo,
+    });
+
+    return acc;
+  }, {} as Record<number, CountriesWithLeagues>);
+
+  // Convertir el objeto agrupado en un array
+  return Object.values(grouped);
+};
+
+
+export const statusFilters: Record<number, (match: any) => boolean> = {
+  0: () => true, // Mostrar todos los partidos
+  1: (match: any) => match.matchInfo.live === '1', // Mostrar partidos en vivo
+  2: (match: any) => match.matchInfo.status === '', // Mostrar partidos próximos
+  3: (match: any) => match.matchInfo.status === 'Finalizado', // Mostrar partidos terminados
+};
+
---
+++ infrastructure/utils/remap.ts
@@ -0,0 +1,53 @@
+import { OriginalApiData } from "@/types/Api.model";
+import { RemapFunctionType } from "@/types/Functions";
+import { CountriesWithLeagues, Country, Favorites, League } from "@/types/GameData";
+
+export const remapFavorites: RemapFunctionType<Favorites, OriginalApiData> = (data) => {
+    return data.map((item)  => ({
+        matchInfo: {
+            date: item.match_date,
+            time: item.match_time,
+            league: item.league_name,
+            logo: item.league_logo,
+            live: item.match_live,
+            status: item.match_status,
+            id: item.match_id,
+        },
+        teamHome: {
+            id: item.match_hometeam_id,
+            name: item.match_hometeam_name,
+            srcLogo: item.team_home_badge,
+            score: item.match_hometeam_score,
+        },
+        teamAway: {
+            id: item.match_awayteam_id,
+            name: item.match_awayteam_name,
+            srcLogo: item.team_away_badge,
+            score: item.match_awayteam_score,
+        }
+    }));
+}
+
+export const remapLeaguesByCountry: RemapFunctionType<CountriesWithLeagues, OriginalApiData> = (data) => {
+    return data.reduce((accumulator: Record<string, CountriesWithLeagues>, item) => {
+        const country: Country = {
+            id: item.country_id,
+            name: item.country_name,
+            logo: item.country_logo,
+        };
+        const league: League = {
+            id: item.league_id,
+            name: item.league_name,
+            logo: item.league_logo,
+        };
+
+        if (!accumulator[country.id]) {
+            accumulator[country.id] = {
+                country,
+                leagues: []
+            };
+        }
+        accumulator[country.id].leagues.push(league);
+        return accumulator;
+    }, {} as Record<string, CountriesWithLeagues>);
+}
