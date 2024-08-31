204ca9c9462e5e08e05e7cc2b0e134b45c2eb7ce
Surialpha <sgp_96@gmail.com>
2024-08-31 12:25:08 -0500

    Commit para obligarte a no dejar deuda tecnica en el codigo que usas, de esta manera tu entorno de desarrollo de obligar a usar algunas reglas que suelen estar configuradas en entornos productivos

--- components/Sections/MatchesSection.tsx
+++ components/Sections/MatchesSection.tsx
@@ -1,6 +1,5 @@
 import { useCustomData } from '@/hooks/useCustomData';
-import { RemapFunctionType, RemappedDataType } from '@/types/GameData';
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useState } from 'react'
 import CardMatch from '../Cards/CardMatch/CardMatch';
 import MatchesList from './MatchesList';
 
@@ -25,7 +24,7 @@ export default function MatchesSection() {
                 <div className="relative">
                     <input onChange={(e)=> handleDate(e.currentTarget.value)} className="bg-projectGrays-500 outline-none rounded-md p-1 text-xs font-semibold w-full" type="date" />
                     <svg xmlns="http://www.w3.org/2000/svg" className="absolute right-2 top-1/2 w-4 h-4 text-gray-400 transform -translate-y-1/2 pointer-events-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                     </svg>
                 </div>
             </nav>
--- utils/fetchers.ts
+++
@@ -1,9 +0,0 @@
-export const customFetcher = async (url: string, remapFunction: Function) => {
-  const response = await fetch(url);
-  const data = await response.json();
-
-  // Aplicar la función de remapeo proporcionada
-  const remappedData = remapFunction ? remapFunction(data) : data;
-
-  return remappedData;
-};
